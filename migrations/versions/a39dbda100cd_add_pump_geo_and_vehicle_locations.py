31af46efa1bb9a1801e3b4e6560d090a4369a57f
Ankushsph <ankushscs645852@gmail.com>
2026-01-11 17:15:30 +0530

    Fix migration chain after removing ClinkerOpt; set a39dbda100cd down_revision to None

--- migrations/versions/a39dbda100cd_add_pump_geo_and_vehicle_locations.py
+++ migrations/versions/a39dbda100cd_add_pump_geo_and_vehicle_locations.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a39dbda100cd'
-down_revision = 'e3ff51718168'
+down_revision = None
 branch_labels = None
 depends_on = None
 
